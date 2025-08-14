2892ba7df232f58b07c53b774f23633ff0a0ed0c
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2025-08-14 10:23:22 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Analytics", "Machine Learning","Deep Learning", "frontend development"],
+    strings: ["Generative AI", "Data Analytics", "Machine Learning","Deep Learning", "Frontend development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
